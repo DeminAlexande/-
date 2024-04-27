512998ea71c2d0119854a574cb992a22da6d4664
DeminAlexande <167640319+DeminAlexande@users.noreply.github.com>
2024-04-27 19:12:35 +0300

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,7 @@
+import random
+a = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+b = int(input("Введите длину пароля "))
+c = " "
+for i in range(b):
+    c += random.choice(a)
+print(c) 
